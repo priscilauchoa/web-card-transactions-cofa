0fc6e282709cbc767116979c5bcfa29ce0c7d1b1
Priscila Flores <priscilauchoaa@gmail.com>
2024-04-24 14:28:02 +0200

    feat: clear filter input on card change and create styled components

--- src/components/TransactionFilter/TransactionFilter.tsx
+++ src/components/TransactionFilter/TransactionFilter.tsx
@@ -1,23 +1,50 @@
-import React, { ChangeEvent } from 'react'
-import { Transaction } from '../../ApiClient';
+import React, { useEffect, useRef, ChangeEvent } from 'react'
+import { Card, Transaction } from '../../ApiClient';
+import styled from "@emotion/styled"
+
+const InputFilter = styled.input`
+  background-color: white;
+  border-radius: 4px;
+  box-sizing: border-box;
+  color: black;
+  margin: 8px 0 30px 0;
+  padding: 0.6em; 
+  width: 100%;
+`
+const LabelFilter = styled.label`
+  color: black;
+  display: flex;
+  font-size: 12px;
+  font-weight:bold;
+  margin-top: 20px;
+  width: 100%;
+`
 
 interface TransactionFilterProps {
+  card: Card;
   transactions: Array<Transaction>;
   onFiltered: (transactions: Array<Transaction>) => void;
-  amount: string;
 }
 
-function TransactionFilter({ transactions, onFiltered }: TransactionFilterProps) {
+function TransactionFilter({ card, transactions, onFiltered }: TransactionFilterProps) {
+  const inputRef = useRef<HTMLInputElement>(null)
 
-  function handleFilterAmount(e: ChangeEvent<HTMLInputElement>) {
+  useEffect(() => {
+    if (!inputRef.current) return
+    inputRef.current.value = ""
+  }, [card, inputRef])
+
+
+  const handleFilterAmount = (e: ChangeEvent<HTMLInputElement>) => {
     const amount = parseFloat(e.target.value)
     const filtered = amount ? transactions.filter((value) => value.amount >= amount) : transactions
     onFiltered(filtered)
   }
+
   return (
     <>
-      <label>Amount Filter</label>
-      <input placeholder='Amount' type="number" onChange={handleFilterAmount} />
+      <LabelFilter>Amount Filter</LabelFilter>
+      <InputFilter ref={inputRef} placeholder='Amount' type="number" onChange={handleFilterAmount} />
     </>
   )
 }
